5a43e794bb811f4e455e9b3c7ba957e894a7cd11
FelipeTeixeira3 <2022498@student.cct.ie>
2023-05-12 12:30:36 +0000

    created 2 pages,  will have a link between them

---
+++ mobile_ca1/src/app/second/second.page.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second',
+  templateUrl: './second.page.html',
+  styleUrls: ['./second.page.scss'],
+})
+export class SecondPage {
+  name = '';
+  email = '';
+  favoriteConsole = '';
+  favoriteGame = '';
+  isSubmitted = false;
+
+  onSubmit() {
+    this.isSubmitted = true;
+  }
+
+  ngOnInit() {
+  }
+
+  
+
+}
